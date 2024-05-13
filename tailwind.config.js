d08df1eb34ab06e7ea632c51ea37982bfa97fac6
ewazna <e.m.wazna@gmail.com>
2024-05-13 20:39:50 +0200

    configuration of tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'pm-white': '#FCFAF8',
+        'pm-black': '#140F1F',
+        'pm-grey': {
+          'light': '#F6F6F6',
+          'base': '#E6E6E6',
+          'dark': '#C4C4C4',
+          'darker': '#A5A5A5'
+        },
+        'pm-orange': {
+          'lighter': '#FFF3E0',
+          'light': '#FFB44C',
+          'base': '#FF8A00',
+          'dark': '#FA4B04'
+        },
+        'pm-green': {
+          'lighter': '#F7FCE6',
+          'light': '#C5E84C',
+          'base': '#A1BB0D',
+          'dark': '#7A7D00'
+        },
+        'pm-success': '#3ABB0D',
+        'pm-error': '#FF2E2E'
+      },
+      fontFamily: {
+        roboto: '"Roboto", sans-serif',
+        raleway: '"Raleway", sans-serif'
+      }
+    },
+  },
+  plugins: [],
+}
+
