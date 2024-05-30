0b7f8d57a5dc0abb73071dbf868aedaae0eec0cb
ewazna <e.m.wazna@gmail.com>
2024-05-30 15:06:07 +0200

    changes in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,29 +13,47 @@ export default {
           'light': '#F6F6F6',
           'base': '#E6E6E6',
           'dark': '#C4C4C4',
-          'darker': '#A5A5A5'
+          'darker': '#A5A5A5',
+          'hover': '#E5E2E3',
         },
         'pm-orange': {
           'lighter': '#FFF3E0',
           'light': '#FFB44C',
           'base': '#FF8A00',
-          'dark': '#FA4B04'
+          'dark': '#FA4B04',
+          'hover':'#E67C00',
         },
         'pm-green': {
           'lighter': '#F7FCE6',
           'light': '#C5E84C',
           'base': '#A1BB0D',
-          'dark': '#7A7D00'
+          'dark': '#7A7D00',
+          'hover':'#91A90C',
         },
         'pm-success': '#3ABB0D',
-        'pm-error': '#FF2E2E'
+        'pm-error': {
+          'base':'#FF2E2E',
+          'hover':'#E62929',
+        }
       },
       fontFamily: {
         roboto: '"Roboto", sans-serif',
         raleway: '"Raleway", sans-serif'
-      }
-    },
-  },
-  plugins: [],
+      },
+      dropShadow: {
+        'xl': '0 4px 4px rgba(0, 0, 0, 0.25)'
+      },
+      boxShadow: {
+        'inner': '0px 3px 5px 0px rgba(0,0,0,0.15) inset'
+      },
+      fontSize: {
+        xs: '0.75rem',
+        s: '1rem',
+        m: '1.25rem',
+        l:'1.875rem',
+        xl: '2.5rem',
+      },
+      plugins: [],
+    }
+  }
 }
-
