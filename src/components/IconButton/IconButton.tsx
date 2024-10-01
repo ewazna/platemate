d9c03666fc684be7697a70e1934445d9699f8827
ewazna <e.m.wazna@gmail.com>
2024-10-01 19:33:34 +0200

    add forwardRef to IconButton and add focus styling

--- src/components/IconButton/IconButton.tsx
+++ src/components/IconButton/IconButton.tsx
@@ -1,10 +1,14 @@
-import className from "classnames";
+import React, { ForwardedRef, PropsWithChildren } from "react";
+
 import { twMerge } from "tailwind-merge";
 import { IconButtonProps } from "./IconButtonProps";
-import { PropsWithChildren } from "react";
 import { GoSync } from "react-icons/go";
+import className from "classnames";
 
-function IconButton(props: PropsWithChildren<IconButtonProps>) {
+const IconButton = React.forwardRef<
+  HTMLButtonElement,
+  PropsWithChildren<IconButtonProps>
+>((props, ref: ForwardedRef<HTMLButtonElement>) => {
   const {
     children,
     primary,
@@ -21,13 +25,18 @@ function IconButton(props: PropsWithChildren<IconButtonProps>) {
 
   const classes = twMerge(
     className(
-      "flex items-center px-2 py-2 rounded-full hover:bg-pm-grey-base active:shadow-inner",
+      "flex items-center px-2 py-2 rounded-full hover:bg-pm-grey-base active:shadow-inner ",
+      "focus-visible:outline focus-visible:outline-2 ",
       {
-        "bg-transparent text-pm-orange-base": primary,
-        "bg-trannsparent text-pm-green-base": secondary,
-        "bg-transparent text-pm-black": isBasic,
-        "bg-transparent text-pm-error-base": error,
-        "drop-shadow-xl active:drop-shadow-none": raised,
+        "bg-transparent text-pm-orange-base focus-visible:outline-pm-orange-base ":
+          primary,
+        "bg-trannsparent text-pm-green-base focus-visible:outline-pm-green-base ":
+          secondary,
+        "bg-transparent text-pm-black focus-visible:outline-pm-white ": isBasic,
+        "bg-transparent text-pm-error-base focus-visible:outline-pm-error-base ":
+          error,
+        "drop-shadow-xl active:drop-shadow-none focus-visible:outline-4 focus-visible:outline-offset-2":
+          raised,
         "bg-pm-orange-base text-pm-white hover:bg-pm-orange-hover active:bg-pm-white active:text-pm-orange-base":
           raised && primary,
         "bg-pm-green-base text-pm-white hover:bg-pm-green-hover active:bg-pm-white active:text-pm-green-base":
@@ -37,7 +46,7 @@ function IconButton(props: PropsWithChildren<IconButtonProps>) {
         "bg-pm-error-base text-pm-white hover:bg-pm-error-hover active:bg-pm-white active:text-pm-error-base":
           raised && error,
         "opacity-80 pointer-events-none": loading,
-        "opacity-20 pointer-events-none": disabled,
+        "opacity-40 pointer-events-none focus-visible:outline-0": disabled,
       },
       rest.className
     )
@@ -64,10 +73,10 @@ function IconButton(props: PropsWithChildren<IconButtonProps>) {
   checkTypeVariation(props);
 
   return (
-    <button {...rest} className={classes}>
+    <button ref={ref} {...rest} className={classes}>
       {loading ? <GoSync className="animate-spin" /> : children}
     </button>
   );
-}
+});
 
 export default IconButton;
