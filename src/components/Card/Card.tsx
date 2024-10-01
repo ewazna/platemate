a9a6bb056391ac07df8596792dde77b4f8b859d9
ewazna <e.m.wazna@gmail.com>
2024-10-01 19:42:15 +0200

    add forwardRef to Card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,20 +1,25 @@
+import React, { ForwardedRef } from "react";
+
 import className from "classnames";
 import { twMerge } from "tailwind-merge";
 import { CardProps } from "./CardProps";
 
-function Card({ children, ...rest }: CardProps) {
-  const classes = twMerge(
-    className(
-      "w-full p-4 rounded-2xl drop-shadow-[0_-5px_20px_rgba(0,0,0,0.15)]",
-      rest.className
-    )
-  );
+const Card = React.forwardRef<HTMLDivElement, CardProps>(
+  (props, ref: ForwardedRef<HTMLDivElement>) => {
+    const { children, ...rest } = props;
+    const classes = twMerge(
+      className(
+        "w-full p-4 rounded-2xl drop-shadow-[0_-5px_20px_rgba(0,0,0,0.15)]",
+        rest.className
+      )
+    );
 
-  return (
-    <div {...rest} className={classes}>
-      {children}
-    </div>
-  );
-}
+    return (
+      <div {...rest} className={classes} ref={ref}>
+        {children}
+      </div>
+    );
+  }
+);
 
 export default Card;
