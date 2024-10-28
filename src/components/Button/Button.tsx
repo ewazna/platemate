470e9f3c581dfe71b9d8ec0319e632e26c0b3742
ewazna <e.m.wazna@gmail.com>
2024-10-28 19:21:27 +0100

    small improvements in Button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,100 +1,90 @@
 import classNames from "classnames";
 import { twMerge } from "tailwind-merge";
 import { ButtonProps } from "./ButtonProps";
-import { PropsWithChildren } from "react";
+import React, { ForwardedRef, PropsWithChildren } from "react";
 import { GoSync } from "react-icons/go";
 
-function Button(props: PropsWithChildren<ButtonProps>) {
-  const {
-    children,
-    primary,
-    secondary,
-    basic,
-    error,
-    raised,
-    underlined,
-    outlined,
-    disabled,
-    loading,
-    ...rest
-  } = props;
+const Button = React.forwardRef<HTMLButtonElement, PropsWithChildren<ButtonProps>>(
+  (props, ref: ForwardedRef<HTMLButtonElement>) => {
+    const {
+      children,
+      primary,
+      secondary,
+      basic,
+      error,
+      raised,
+      underlined,
+      outlined,
+      disabled,
+      loading,
+      ...rest
+    } = props;
 
-  const isRaised = raised || (!underlined && !outlined && !raised);
-  const isBasic = basic || (!primary && !secondary && !error && !basic);
+    const isRaised = raised || (!underlined && !outlined && !raised);
+    const isBasic = basic || (!primary && !secondary && !error && !basic);
 
-  const classes = twMerge(
-    classNames(
-      "flex items-center px-16 py-2 rounded-full font-roboto font-medium ",
-      "focus-visible:outline focus-visible:outline-2",
-      {
-        "bg-pm-orange-base text-pm-white focus-visible:outline-pm-orange-base ":
-          primary,
-        "bg-pm-green-base text-pm-white focus-visible:outline-pm-green-base":
-          secondary,
-        "bg-pm-white text-pm-black focus-visible:outline-pm-white": isBasic,
-        "bg-pm-error-base text-pm-white focus-visible:outline-pm-error-base":
-          error,
-        "drop-shadow-xl uppercase active:drop-shadow-none active:shadow-inner":
-          isRaised,
-        "notmobile:hover:bg-pm-orange-hover": isRaised && primary,
-        "notmobile:hover:bg-pm-green-hover": isRaised && secondary,
-        "notmobile:hover:bg-pm-grey-hover": isRaised && isBasic,
-        "notmobile:hover:bg-pm-error-hover": isRaised && error,
-        "bg-transparent normal-case notmobile:hover:underline active:shadow-none":
-          underlined,
-        "text-pm-orange-base px-1": underlined && primary,
-        "text-pm-green-base px-1": underlined && secondary,
-        "text-pm-black px-1 active:text-pm-orange-base": underlined && isBasic,
-        "lowercase bg-transparent border border-pm-black m-1 px-3 py-1 drop-shadow-none":
-          outlined,
-        "border-pm-orange-base text-pm-orange-base notmobile:hover:bg-pm-orange-lighter":
-          outlined && primary,
-        "border-pm-green-base text-pm-green-base notmobile:hover:bg-pm-green-lighter":
-          outlined && secondary,
-        "border-pm-black text-pm-black notmobile:hover:bg-pm-grey-light":
-          outlined && isBasic,
-        "border-pm-error-base text-pm-error-base notmobile:hover:bg-pm-error-lighter":
-          outlined && error,
-        "opacity-80 pointer-events-none": loading,
-        "opacity-20 pointer-events-none": disabled,
-      },
-      rest.className
-    )
-  );
+    const classes = twMerge(
+      classNames(
+        "flex items-center justify-center px-8 py-2 font-roboto font-medium focus-visible:outline-0",
+        {
+          "bg-pm-orange-base text-pm-white focus-visible:outline-pm-orange-base": primary,
+          "bg-pm-green-base text-pm-white focus-visible:outline-pm-green-base": secondary,
+          "bg-pm-white text-pm-black focus-visible:outline-pm-orange-base active:bg-pm-grey-base active:bg-opacity-40 active:shadow-inner":
+            isBasic,
+          "bg-pm-error-base text-pm-white focus-visible:outline-pm-error-base": error,
+          "rounded-full drop-shadow-xl uppercase active:drop-shadow-none active:shadow-inner focus-visible:outline focus-visible:outline-offset-2 focus-visible:outline-2":
+            isRaised,
+          "notmobile:hover:bg-pm-orange-hover": isRaised && primary,
+          "notmobile:hover:bg-pm-green-hover": isRaised && secondary,
+          "notmobile:hover:bg-pm-grey-hover": isRaised && isBasic,
+          "notmobile:hover:bg-pm-error-hover": isRaised && error,
+          "bg-transparent normal-case notmobile:hover:underline active:shadow-none focus-visible:border-b-2 focus-visible:-translate-y-1":
+            underlined,
+          "text-pm-orange-base px-1 active:text-pm-black active:font-black focus-visible:border-pm-orange-base":
+            underlined && primary,
+          "text-pm-green-base px-1 active:text-pm-black active:font-black focus-visible:border-pm-green-base":
+            underlined && secondary,
+          "text-pm-black px-1 active:text-pm-orange-base focus-visible:border-pm-orange-base":
+            underlined && isBasic,
+          "rounded-full lowercase bg-transparent border border-pm-black m-1 px-3 py-1 drop-shadow-none focus-visible:outline focus-visible:outline-offset-2 focus-visible:outline-2":
+            outlined,
+          "border-pm-orange-base text-pm-orange-base notmobile:hover:bg-pm-orange-lighter":
+            outlined && primary,
+          "border-pm-green-base text-pm-green-base notmobile:hover:bg-pm-green-lighter":
+            outlined && secondary,
+          "border-pm-black text-pm-black notmobile:hover:bg-pm-grey-light": outlined && isBasic,
+          "border-pm-error-base text-pm-error-base notmobile:hover:bg-pm-error-lighter":
+            outlined && error,
+          "opacity-80 pointer-events-none": loading,
+          "opacity-20 pointer-events-none": disabled,
+        },
+        rest.className,
+      ),
+    );
 
-  function checkTypeVariation({
-    primary,
-    secondary,
-    basic,
-    error,
-  }: ButtonProps) {
-    const count =
-      Number(!!primary) +
-      Number(!!secondary) +
-      Number(!!basic) +
-      Number(!!error);
-    if (count > 1) {
-      throw new Error(
-        "Only one of primary, secondary, basic, error can be true"
-      );
+    function checkTypeVariation({ primary, secondary, basic, error }: ButtonProps) {
+      const count = Number(!!primary) + Number(!!secondary) + Number(!!basic) + Number(!!error);
+      if (count > 1) {
+        throw new Error("Only one of primary, secondary, basic, error can be true");
+      }
     }
-  }
 
-  function checkStyleVariation({ raised, underlined }: ButtonProps) {
-    const count = Number(!!raised) + Number(!!underlined);
-    if (count > 1) {
-      throw new Error("Only one of raised, underlined can be true");
+    function checkStyleVariation({ raised, underlined }: ButtonProps) {
+      const count = Number(!!raised) + Number(!!underlined);
+      if (count > 1) {
+        throw new Error("Only one of raised, underlined can be true");
+      }
     }
-  }
 
-  checkTypeVariation(props);
-  checkStyleVariation(props);
+    checkTypeVariation(props);
+    checkStyleVariation(props);
 
-  return (
-    <button {...rest} className={classes}>
-      {loading ? <GoSync className="animate-spin" /> : children}
-    </button>
-  );
-}
+    return (
+      <button type="button" {...rest} ref={ref} className={classes} disabled={disabled}>
+        {loading ? <GoSync className="animate-spin" /> : children}
+      </button>
+    );
+  },
+);
 
 export default Button;
