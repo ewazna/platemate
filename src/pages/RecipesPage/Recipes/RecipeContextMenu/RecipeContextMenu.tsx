f9e226eff1b6a69d62d731d077091170d4e75d85
ewazna <e.m.wazna@gmail.com>
2024-10-29 17:54:41 +0100

    small fixes in RecipeContextMenu component

--- src/pages/RecipesPage/Recipes/RecipeContextMenu/RecipeContextMenu.tsx
+++ src/pages/RecipesPage/Recipes/RecipeContextMenu/RecipeContextMenu.tsx
@@ -1,17 +1,18 @@
+import { PiTrashSimpleBold, PiHeartStraightBold, PiHeartStraightFill } from "react-icons/pi";
 import ContextMenu from "../../../../components/ContextMenu/ContextMenu";
 import Button from "../../../../components/Button/Button";
 import {
   useChangeRecipeItemMutation,
   useRemoveRecipeItemMutation,
-} from "../../../../store/apis/recipesApi";
-import { PiTrashSimpleBold, PiHeartStraightBold, PiHeartStraightFill } from "react-icons/pi";
+} from "../../../../store/apis/recipes/recipesApi";
 import { RecipeContextMenuProps } from "./RecipeContextMenuProps";
 
 function RecipeContextMenu({ recipe }: RecipeContextMenuProps) {
   const [removeRecipeItem] = useRemoveRecipeItemMutation();
   const [changeRecipeItem] = useChangeRecipeItemMutation();
 
-  const handleRemove = () => {
+  const handleRemove = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
     removeRecipeItem(recipe);
   };
 
@@ -25,7 +26,7 @@ function RecipeContextMenu({ recipe }: RecipeContextMenuProps) {
 
   let heartIcon;
   if (recipe.favourite) {
-    heartIcon = <PiHeartStraightFill className="text-pm-orange-base mr-3" />;
+    heartIcon = <PiHeartStraightFill className="text-pm-orange-base mr-2" />;
   } else {
     heartIcon = <PiHeartStraightBold className="text-pm-orange-base mr-2" />;
   }
@@ -34,15 +35,15 @@ function RecipeContextMenu({ recipe }: RecipeContextMenuProps) {
     <ContextMenu id={"menu-" + recipe._id} className="w-40">
       <Button
         type="button"
-        className="normal-case px-4 drop-shadow-none bg-pm-grey-light text-start"
+        className="normal-case px-3 w-36 h-8 my-1 drop-shadow-none bg-pm-grey-light justify-start text-start"
         onClick={handleAddToFavs}
       >
         {heartIcon}
-        {recipe.favourite ? " Remove from Favs" : " Add to Favs"}
+        {recipe.favourite ? "Unmark Favs" : "Mark Favs"}
       </Button>
       <Button
         type="button"
-        className="normal-case px-4 h-10 drop-shadow-none bg-pm-grey-light"
+        className="normal-case px-3 w-36 h-8 my-1 drop-shadow-none bg-pm-grey-light justify-start"
         onClick={handleRemove}
       >
         <PiTrashSimpleBold className="text-pm-orange-base mr-2" /> Remove
