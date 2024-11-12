82fee5971b71960941f201a7e073c04a9f66aeac
ewazna <e.m.wazna@gmail.com>
2024-11-12 20:25:07 +0100

    add Providers and some global styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import "./index.css";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { store } from "./store";
+import { Provider } from "react-redux";
+import { ToastProvider } from "./components/Toast/ToastProvider.tsx";
+import { BrowsingStateProvider } from "./pages/RecipesPage/BrowsingStateProvider.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <Provider store={store}>
+    <ToastProvider>
+      <BrowsingStateProvider>
+        <App />
+      </BrowsingStateProvider>
+    </ToastProvider>
+  </Provider>,
+);
