7bda877347343a670237e6b7167cbfff91d2298e
ewazna <e.m.wazna@gmail.com>
2024-11-12 20:17:06 +0100

    add animations in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -41,7 +41,8 @@ export default {
         raleway: '"Raleway", sans-serif'
       },
       dropShadow: {
-        'xl': '0 4px 4px rgba(0, 0, 0, 0.25)'
+        'xl': '0 4px 4px rgba(0, 0, 0, 0.25)',
+        'full': '0 0 6px rgba(0, 0, 0, 0.25)',
       },
       boxShadow: {
         'inner': '0px 3px 5px 0px rgba(0,0,0,0.15) inset'
@@ -50,8 +51,105 @@ export default {
         xs: '0.75rem',
         s: '1rem',
         m: '1.25rem',
-        l:'1.875rem',
-        xl: '2.5rem',
+        l:'1.75rem',
+        xl: '2rem',
+        xxl: '2.5rem',
+        xxxl: '3.5rem'
+      },
+      blur:{
+        sm:'3px'
+      },
+      keyframes: {
+        shimmer: {
+          '100%': { transform: 'translateX(100%)' }
+        },
+        showUp:{
+          '0%': {
+            translate: '0 50%',
+            opacity: "0%",
+          },
+          '1%':  {
+            translate: '0 50%',
+            opacity: "100%",
+          },
+          '100%': {
+            translate: '0 0',
+            opacity: "100%",
+          }},
+        dissapear: { 
+          "0%": {
+          opacity: "100%",
+          scale: "100%",
+        },       
+        "100%": {
+          opacity: "0%",
+          scale: "50%"
+        }},
+        slideOut: {
+          '0%': {
+            transform:'translateY(0%)',
+            display: 'inherit'
+          },
+          '99%':  {
+            transform:'translateY(100%)',
+            display: 'inherit'
+          },
+          '100%': {
+            transform:'translateY(100%)',
+            display: 'none'
+          }
+        }, 
+        slideIn: {
+          '0%': {
+            transform:'translateY(100%)',
+            display: 'none'
+          },
+          '1%':  {
+            transform:'translateY(100%)',
+            display: 'inherit'
+          },
+          '100%': {
+            transform:'translateY(0%)',
+            display: 'inherit'
+          }
+        },
+        fadeOut: {
+          '0%': {
+            opacity:'80%',
+            display: 'inherit'
+          },
+          '99%':  {
+            opacity:'0%',
+            display: 'inherit'
+          },
+          '100%': {
+            opacity:'0%',
+            display: 'none'
+          }
+        }, 
+        fadeIn: {
+          '0%': {
+            opacity:'0%',
+            display: 'none'
+          },
+          '1%':  {
+            opacity:'0%',
+            display: 'inherit'
+          },
+          '100%': {
+            opacity:'80%',
+            display: 'inherit'
+          },
+        }
+      },
+      animation: {
+        shimmer: 'shimmer 1.5s infinite',
+        showUp: 'showUp 1s',
+        dissapear: 'dissapear 1s',
+        slideIn: 'slideIn 0.3s',
+        slideOut: 'slideOut 0.3s',
+        fadeIn: 'fadeIn 0.3s',
+        fadeOut: 'fadeOut 0.3s', 
       },
       plugins: [],
     }
