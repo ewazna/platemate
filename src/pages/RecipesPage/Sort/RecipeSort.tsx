f2199e329f829b63f090e65ddc7bb7b73a6bf4d9
ewazna <e.m.wazna@gmail.com>
2024-12-17 19:29:29 +0100

    sorting options reworked for desktop view

--- src/pages/RecipesPage/Sort/RecipeSort.tsx
+++ src/pages/RecipesPage/Sort/RecipeSort.tsx
@@ -1,6 +1,5 @@
 import { createPortal } from "react-dom";
 import { TiArrowSortedDown, TiArrowSortedUp } from "react-icons/ti";
-import { TbStar } from "react-icons/tb";
 import { MdUpdate } from "react-icons/md";
 import { RecipeSortProps } from "./RecipeSortProps";
 import Card from "../../../components/Card/Card";
@@ -17,12 +16,11 @@ function RecipeSort({
   const sortOptions = [
     { label: "A to Z", value: "AZ", icon: <TiArrowSortedUp /> },
     { label: "Z to A", value: "ZA", icon: <TiArrowSortedDown /> },
-    { label: "Best rate", value: "BestRate", icon: <TbStar /> },
     { label: "Latest", value: "Latest", icon: <MdUpdate /> },
   ];
 
   const cardClasses =
-    "absolute top-[156px] right-[16px] w-40 p-2 " +
+    "absolute top-[152px] right-[16px] w-40 p-2 md:top-[200px] md:right-[20px] " +
     (isSortFormShown ? "display-[inherit]" : "hidden");
 
   if (!sortConfig) {
@@ -47,7 +45,7 @@ function RecipeSort({
                 onChange={handleChange}
                 className="flex flex-nowrap flex-col"
                 selectionColor="bg-pm-grey-base"
-                bgOptionColor=""
+                bgOptionColor="hover:bg-pm-grey-hover"
                 selectionIcons={true}
               />
             </Card>
