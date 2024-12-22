46b786a0106cc9a7e5da3c7b56e42f0adec4e286
ewazna <e.m.wazna@gmail.com>
2024-12-22 11:26:27 +0100

    remove TextInput component

--- src/components/TextInput/TextInputProps.ts
+++
@@ -1,8 +0,0 @@
-import type { ComponentPropsWithoutRef } from "react";
-
-export interface TextInputProps extends ComponentPropsWithoutRef<"input"> {
-  children?: string;
-  basic?: boolean;
-  raised?: boolean;
-  icon?: React.ReactNode;
-}
